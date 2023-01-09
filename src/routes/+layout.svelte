0258f2a785fd660312786e4f516ecbde7a9ba188
Vadim Egorov <vadegor00@gmail.com>
2023-01-08 15:59:36 -0900

    changed configurations and added tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import '../app.css';
+</script>
+
+<div class="min-h-screen text-white bg-background">
+  <slot />
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        background: '#282C33',
+      },
+    },
+  },
+  plugins: [],
+};
